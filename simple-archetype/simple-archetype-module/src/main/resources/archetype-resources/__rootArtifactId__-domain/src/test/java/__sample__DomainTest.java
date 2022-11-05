61b27088bcbf1d231a2f9e18f5498783cce73d62
loyayz <loyayz@foxmail.com>
2022-11-06 00:43:49 +0800

    feat(archetype): add test resources

---
+++ simple-archetype/simple-archetype-module/src/main/resources/archetype-resources/__rootArtifactId__-domain/src/test/java/__sample__DomainTest.java
@@ -0,0 +1,14 @@
+package ${package}.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ${sample}DomainTestApplication.class)
+public class ${sample}DomainTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
---
+++ simple-archetype/simple-archetype-module/src/main/resources/archetype-resources/__rootArtifactId__-domain/src/test/java/__sample__DomainTestApplication.java
@@ -0,0 +1,13 @@
+package ${package}.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ${sample}DomainTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(${sample}DomainTestApplication.class, args);
+    }
+
+}
